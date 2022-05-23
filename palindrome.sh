def600a0b58753975086d5fd143dd80c5c7e6f15
Teemu Homi <53630914+houmii@users.noreply.github.com>
2022-05-23 22:20:43 +0300

    Add files via upload

---
+++ palindrome.sh
@@ -0,0 +1,37 @@
+#/bin/sh
+#
+clear
+echo -n " 
+ █▀▄ ▄▀▄ █   █ █▄ █ █▀▄ █▀▄ ▄▀▄ █▄ ▄█ ██▀   ▄▀▀ █▄█ ██▀ ▄▀▀ █▄▀ ██▀ █▀▄
+ █▀  █▀█ █▄▄ █ █ ▀█ █▄▀ █▀▄ ▀▄▀ █ ▀ █ █▄▄   ▀▄▄ █ █ █▄▄ ▀▄▄ █ █ █▄▄ █▀▄
+	LINUX-BASICS 3 OP | Shell Script exercise | Teemu Homi
+
+"
+sleep 1
+                                                                      
+echo -n 'Type a word here to see if it is a palindrome -> '
+
+read vstr
+sleep 1
+for i in $(seq 0 ${#vstr})
+do
+	rvstr=${vstr:$i:1}${rvstr}
+done
+sleep 1
+echo "
+Your word $vstr is $rvstr on backwards!"
+
+if [ "$vstr" = "$rvstr" ]
+then
+	sleep 2
+	echo "
+WHOA! THE WORD YOU GAVE ME IS PALINDROME!
+"
+
+else
+	sleep 2
+	echo "
+I'm sorry, but the word you gave me isn't palindrome.
+"
+
+fi
