50c8de1d741453d1c4341461e37b191bf9b91542
Teemu Homi <53630914+houmii@users.noreply.github.com>
2022-09-03 11:52:11 +0200

    bubble_sort.sh

--- bubble_sort.sh
+++ bubble_sort.sh
@@ -1,3 +1,13 @@
+# Make Bourne Shell program using bubble sort algorithm to sort numbers given by user into the ascending order.
+# Program asks first numbers of elements to sort and then asks those numbers one by one.
+# Program prints numbers in original and sorted order.
+# Bubble Sort is a simple sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order. See details:
+# https://en.wikipedia.org/wiki/Bubble_sort
+
+
+#!/bin/sh
+#
+clear
 echo "Enter size of array"; read  arraysize; #get the size of array from user.
 echo "Enter the array eg: 2 3 4 5 6"; read -a arr; #get the array form user eg: 2 3 4 5 6
 echo "Original array is: ${arr[*]}";
